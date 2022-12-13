17bc08f278c4ed197ef72e5d5759a25f85656875
Ajay Joshi <joshi28@wisc.edu>
2022-12-13 16:26:32 -0600

    Unrolling loop + warpreduce

--- Backward/softmax_backward.cu
+++ Backward/softmax_backward.cu
@@ -76,13 +76,47 @@ __global__ void fused_softmax(const float *P_, const float* dP_, float* dS_, int
     }
 }
 
-__device__ void warpReduce(volatile float* sdata) {
-    sdata[threadIdx.x] += sdata[threadIdx.x + 32];
-    sdata[threadIdx.x] += sdata[threadIdx.x + 16];
-    sdata[threadIdx.x] += sdata[threadIdx.x + 8];
-    sdata[threadIdx.x] += sdata[threadIdx.x + 4];
-    sdata[threadIdx.x] += sdata[threadIdx.x + 2];
-    sdata[threadIdx.x] += sdata[threadIdx.x + 1];
+__device__ inline void warpReduce(volatile float* temp_sums) {
+    temp_sums[threadIdx.x] += temp_sums[threadIdx.x + 32];
+    temp_sums[threadIdx.x] += temp_sums[threadIdx.x + 16];
+    temp_sums[threadIdx.x] += temp_sums[threadIdx.x + 8];
+    temp_sums[threadIdx.x] += temp_sums[threadIdx.x + 4];
+    temp_sums[threadIdx.x] += temp_sums[threadIdx.x + 2];
+    temp_sums[threadIdx.x] += temp_sums[threadIdx.x + 1];
+    __syncthreads();
+}
+
+__global__ void fused_softmax2(const float *P_, const float* dP_, float* dS_, int N, int batch_size, int num_heads) {
+    int idx1 = blockIdx.x;
+    const float* P = P_ + idx1 * N * N, *dP = dP_ + idx1 * N * N;
+    float* dS = dS_ + idx1 * N * N;
+
+    extern __shared__ float shMem[];
+    float* shP = shMem;
+    float* shPdP = shP + N;
+    float* temp_sums = shPdP + N;
+
+    // Each thread computes sum of a row
+    for(int i = 0; i < N; i++) {
+        // load row to shMem
+        shP[threadIdx.x] = P[i * N + threadIdx.x];
+        shPdP[threadIdx.x] = shP[threadIdx.x] * dP[i * N + threadIdx.x];
+        __syncthreads();
+        temp_sums[threadIdx.x] = shPdP[threadIdx.x] + shPdP[threadIdx.x + blockDim.x/2];
+        __syncthreads();
+
+        // reduce
+        if(blockDim.x >= 512){ temp_sums[threadIdx.x] += temp_sums[threadIdx.x + 256]; __syncthreads(); }
+        if(blockDim.x >= 256){ temp_sums[threadIdx.x] += temp_sums[threadIdx.x + 128]; __syncthreads(); }
+        if(blockDim.x >= 128){ temp_sums[threadIdx.x] += temp_sums[threadIdx.x + 64]; __syncthreads(); }
+        warpReduce(temp_sums);
+
+        float rowsum = temp_sums[0];
+
+        // write to dS
+        dS[i * N + threadIdx.x] = shPdP[threadIdx.x] - shP[threadIdx.x] * rowsum;
+        __syncthreads();
+    }
 }
 
 // Assume N = 1024 = blockDim.x
@@ -92,7 +126,7 @@ __host__ void softmax_backward2(const float *P, const float* dP, float* dS, floa
     int blocks = batch_size * num_heads;
     int shared_memory_size = 6 * N * sizeof(float);
 
-    fused_softmax<<<blocks, threads, shared_memory_size>>>(P, dP, dS, N, batch_size, num_heads);
+    fused_softmax2<<<blocks, threads, shared_memory_size>>>(P, dP, dS, N, batch_size, num_heads);
 }
 
 __host__ void softmax_backward1(const float *P, const float* dP, float* dS, float* rowsums, int N, int batch_size, int num_heads) {
